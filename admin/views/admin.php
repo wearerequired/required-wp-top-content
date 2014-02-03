b01b0b4400d8dc364e52a7b8c1b57b31587971d1
Stefan Pasch <stefan.pasch@gmail.com>
2014-02-03 15:46:23 +0100

    Added plugin base.

---
+++ admin/views/admin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   required-wp-top-content
+ * @author    Stefan Pasch <stefan@required.ch>
+ * @license   GPL-2.0+
+ * @link      http://required.ch
+ * @copyright 2014 required gmbh
+ */
+?>
+
+<div class="wrap">
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+    <form method="post" action="options.php">
+        <?php
+
+        settings_fields( 'required-wp-top-content-options' );
+
+        do_settings_sections( 'required-wp-top-content' );
+
+        ?>
+        <?php submit_button(); ?>
+    </form>
+
+</div>
