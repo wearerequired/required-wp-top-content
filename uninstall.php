23227b4e97f62836eaf15d9ebc0258bfabbc1d59
Dominik Schilling <dominikschilling+git@gmail.com>
2016-11-04 22:16:38 +0100

    Update uninstall.php to include all meta and options

--- uninstall.php
+++ uninstall.php
@@ -9,19 +9,21 @@
  * @copyright 2014 required gmbh
  */
 
-// If uninstall not called from WordPress, then exit
+// If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// remove existing pageviews for all contents
+// Remove existing pageviews for all posts.
 delete_post_meta_by_key( 'rplus_top_content_pageviews' );
 delete_post_meta_by_key( 'rplus_top_content_visits' );
+delete_post_meta_by_key( 'topcontent_exclude' );
 
-// remove options
+// Remove options.
+delete_option( 'rplus_topcontent_options_ga_auth_type' );
 delete_option( 'rplus_topcontent_options_ga_access_token' );
 delete_option( 'rplus_topcontent_options_sync_days' );
 delete_option( 'rplus_topcontent_options_ga_propertyid' );
-delete_option( 'rplus_topcontent_options_ga_devkey' );
 delete_option( 'rplus_topcontent_options_ga_client_id' );
 delete_option( 'rplus_topcontent_options_ga_client_secret' );
+delete_option( 'rplus_topcontent_options_ga_access_code' );
